967cd0c2e623bd8ad29d5e421d50c646182f2676
Lukas Boelling <lukasboelling002@gmail.com>
2021-10-02 04:31:48 -0400

    Fix ordering in enumerate

--- src/logic.py
+++ src/logic.py
@@ -20,7 +20,7 @@ def one_of(*args: Var) -> Var:
     parts = []
     for idx in range(len(args)):
         parts.append(
-            multi_and(a if i == idx else a.negate() for a, i in enumerate(args))
+            multi_and(a if i == idx else a.negate() for i, a in enumerate(args))
         )
     return multi_or(*parts)
 
