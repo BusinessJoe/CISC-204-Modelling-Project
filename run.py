a2243260bd71c648f83c7e516611fc7034e38f6e
Lukas Boelling <lukasboelling002@gmail.com>
2021-11-19 19:08:36 -0500

    Increase recursion limit

--- run.py
+++ run.py
@@ -2,20 +2,26 @@ from nnf import Var
 from src.file_reader import read_file
 from src.lib204 import Encoding
 
+import sys
+
+sys.setrecursionlimit(10 ** 6)
 
 if __name__ == "__main__":
     from pprint import pprint
 
-    with open("data/xml/test2.xml") as f:
+    with open("data/xml/example_long.xml", encoding="utf8") as f:
         T, props = read_file(f)
 
-    print("\nSatisfiable: %s" % T.is_satisfiable())
-    print("# Solutions: %d" % T.count_solutions())
-    print("   Solution:")
-    solution = T.solve()
-    true_part = {k: v for k, v in solution.items() if v}
-    false_part = {k: v for k, v in solution.items() if not v}
-    pprint(true_part)
-    pprint(false_part)
+    satisfiable = T.is_satisfiable()
+    print("\nSatisfiable: %s" % satisfiable)
 
-    print(T.likelihood(props["SA"][1, 1]))
+    if satisfiable:
+        print(f"# Solutions: {T.count_solutions()}")
+        print("Solution:")
+        solution = T.solve()
+        true_part = {k: v for k, v in solution.items() if v}
+        false_part = {k: v for k, v in solution.items() if not v}
+        pprint(true_part)
+        pprint(false_part)
+
+        print(T.likelihood(props["SA"][1, 1]))
