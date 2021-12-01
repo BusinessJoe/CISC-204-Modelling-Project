73a70855d98f581cc4447028e55848c6579f6cc1
Lukas Boelling <lukasboelling002@gmail.com>
2021-12-01 10:40:05 -0500

    Add more test xml files

--- run.py
+++ run.py
@@ -6,10 +6,53 @@ import sys
 
 sys.setrecursionlimit(10 ** 6)
 
+
+def summarize(solution):
+    # Do some filtering
+    solution = {k: v for k, v in solution.items() if isinstance(k, str)}
+
+    alien_coords = {
+        k.split(":")[1] for k, v in solution.items() if v and k.split(":")[0] == "alien"
+    }
+    house_coords = {
+        k.split(":")[1] for k, v in solution.items() if v and k.split(":")[0] == "house"
+    }
+    rail_coords = {
+        k.split(":")[1] for k, v in solution.items() if v and k.split(":")[0] == "rail"
+    }
+
+    pprint({k: v for k, v in solution.items() if v and k.split(":")[0] == "alien"})
+    pprint({k: v for k, v in solution.items() if v and k.split(":")[0] == "house"})
+    pprint(
+        {k: v for k, v in solution.items() if v and k.split(":")[0].startswith("rail")}
+    )
+    pprint(
+        {
+            k: v
+            for k, v in solution.items()
+            if k.split(":")[1] in alien_coords and k.startswith("alien_sat")
+        }
+    )
+    pprint(
+        {
+            k: v
+            for k, v in solution.items()
+            if k.split(":")[1] in house_coords and k.startswith("house_sat")
+        }
+    )
+    pprint(
+        {
+            k: v
+            for k, v in solution.items()
+            if k.split(":")[1] in rail_coords and k.startswith("train_")
+        }
+    )
+
+
 if __name__ == "__main__":
     from pprint import pprint
 
-    with open("data/xml/example_long.xml", encoding="utf8") as f:
+    with open("data/xml/test_long_bend.xml", encoding="utf8") as f:
         T = read_file(f)
 
     satisfiable = T.is_satisfiable()
@@ -22,56 +65,4 @@ if __name__ == "__main__":
 
         solution = T.solve()
 
-        def summarize(solution):
-            # Do some filtering
-            solution = {k: v for k, v in solution.items() if isinstance(k, str)}
-
-            alien_coords = {
-                k.split(":")[1]
-                for k, v in solution.items()
-                if v and k.split(":")[0] == "alien"
-            }
-            house_coords = {
-                k.split(":")[1]
-                for k, v in solution.items()
-                if v and k.split(":")[0] == "house"
-            }
-            rail_coords = {
-                k.split(":")[1]
-                for k, v in solution.items()
-                if v and k.split(":")[0] == "rail"
-            }
-
-            pprint(
-                {k: v for k, v in solution.items() if v and k.split(":")[0] == "alien"}
-            )
-            pprint(
-                {k: v for k, v in solution.items() if v and k.split(":")[0] == "house"}
-            )
-            pprint(
-                {k: v for k, v in solution.items() if v and k.split(":")[0] == "rail"}
-            )
-            pprint(
-                {
-                    k: v
-                    for k, v in solution.items()
-                    if k.split(":")[1] in alien_coords and k.startswith("alien_sat")
-                }
-            )
-            pprint(
-                {
-                    k: v
-                    for k, v in solution.items()
-                    if k.split(":")[1] in house_coords and k.startswith("house_sat")
-                }
-            )
-            pprint(
-                {
-                    k: v
-                    for k, v in solution.items()
-                    if k.split(":")[1] in rail_coords and k.startswith("train_")
-                }
-            )
-
-        for solution in T.models():
-            summarize(solution)
+        summarize(solution)
